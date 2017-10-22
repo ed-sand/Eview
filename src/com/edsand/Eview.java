51dd99c48aa9249e53340c418b8a9603d8490a63
Eduardo Sandoval <ed.sand.pol@gmail.com>
2017-10-22 16:55:46 -0600

    codigo mas bonito

--- src/com/edsand/Eview.java
+++ src/com/edsand/Eview.java
@@ -46,16 +46,12 @@ class ImageViewerFrame extends JFrame {
   public ImageViewerFrame() {
     setTitle("Eview");
     setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-    
     setVariables();
     JButton Buttons[] = {btn_back, btn_next};
-    JLabel Labels[] = {lblBigImage, lblAct, 
-                       lblAnt1,     lblAnt2,  lblAnt3, lblAnt4, lblAnt5,
-                       lblSig1,     lblSig2,  lblSig3, lblSig4,  lblSig5};
     getImagenes();
-    setLabelIcons(Labels);
-    setLabelBounds();
-    addComponets(Buttons, Labels);
+    setLabelIcons(getLabels());
+    setLabelBounds(getLabels());
+    addComponets(Buttons, getLabels());
     addActionListeners();
   }
   
@@ -103,12 +99,9 @@ class ImageViewerFrame extends JFrame {
   
   public void setLabelIcons(JLabel Labels[]) {
     Labels[0].setIcon(imagen_actual.getImagen());
-    
     Labels[1].setIcon(resize(imagen_actual.getImagen()));
-    
     for (int i = 2; i < 7; i++) 
       Labels[i].setIcon(resize(getAnt(imagen_actual, i-1)));
-    
     for (int i = 7; i < Labels.length; i++) 
       Labels[i].setIcon(resize(getSig(imagen_actual, i-6)));
   }
@@ -123,80 +116,61 @@ class ImageViewerFrame extends JFrame {
     return getSig(imagen.getSig(), --recorrido);
   }
   
-  public void setLabelBounds() {
+  public void setLabelBounds(JLabel Labels[]) {
     panel.setLayout(null);
-    int act_x = 1366/2-50;
-    int act_y = 722-200;
-    btn_back.setBounds(1366/2-110, 630, 100, 50);
-    btn_next.setBounds(1366/2+10, 630, 100, 50);
-    lblBigImage.setBounds((1366-600)/2, 30, 600, 462);
-    lblAnt1.setBounds(act_x - 120, act_y, 100, 77);
-    lblSig1.setBounds(act_x + 120, act_y, 100, 77);
-    lblAct.setBounds(act_x, act_y, 100, 77);
-    lblAnt2.setBounds(act_x - 240, act_y, 100, 77);
-    lblSig2.setBounds(act_x + 240, act_y, 100, 77);
-    lblSig3.setBounds(act_x + 360, act_y, 100, 77);
-    lblAnt3.setBounds(act_x - 360, act_y, 100, 77);
-    lblSig4.setBounds(act_x + 480, act_y, 100, 77);
-    lblAnt4.setBounds(act_x - 480, act_y, 100, 77);
-    lblSig5.setBounds(act_x + 600, act_y, 100, 77);
-    lblAnt5.setBounds(act_x - 600, act_y, 100, 77);
+    int middle_x = DEFAULT_WIDTH/2;
+    int act_x = middle_x-50;
+    int act_y = DEFAULT_HEIGHT-200;
+    int x_offset = 120;
+    btn_back.setBounds(middle_x-110, 630, 100, 50);
+    btn_next.setBounds(middle_x+10, 630, 100, 50);
+    Labels[0].setBounds((DEFAULT_WIDTH-600)/2, 30, 600, 462);
+    Labels[1].setBounds(act_x, act_y, 100, 77);
+    for (int i = 2; i < 7; i++) {
+      Labels[i].setBounds(act_x - x_offset, act_y, 100, 77);
+      Labels[i+5].setBounds(act_x + x_offset, act_y, 100, 77);
+      x_offset += 120;
+    }
   }
   
   public void addComponets(JButton Buttons[], JLabel Labels[]) {
     menuBar.add(menu);
     setJMenuBar(menuBar);
-    
     for (JButton button: Buttons) panel.add(button);
     for (JLabel label: Labels) panel.add(label);
-      
     menu.add(openItem);
     this.getContentPane().add(panel);
   }
   
-  public void move_next() {
-    lblBigImage.setIcon(getSig(imagen_actual, 1));
-    
-    lblAct.setIcon(resize(getSig(imagen_actual, 1)));
-    
-    lblAnt1.setIcon(resize(imagen_actual.getImagen()));
-    lblAnt2.setIcon(resize(getAnt(imagen_actual, 1)));
-    lblAnt3.setIcon(resize(getAnt(imagen_actual, 2)));
-    lblAnt4.setIcon(resize(getAnt(imagen_actual, 3)));
-    lblAnt5.setIcon(resize(getAnt(imagen_actual, 4)));
-    
-    lblSig1.setIcon(resize(getSig(imagen_actual, 2)));
-    lblSig2.setIcon(resize(getSig(imagen_actual, 3)));
-    lblSig3.setIcon(resize(getSig(imagen_actual, 4)));
-    lblSig4.setIcon(resize(getSig(imagen_actual, 5)));
-    lblSig5.setIcon(resize(getSig(imagen_actual, 6)));
-    
+  public void move_next(JLabel Labels[]) {
+    Labels[0].setIcon(getSig(imagen_actual, 1));
+    Labels[1].setIcon(resize(getSig(imagen_actual, 1)));
+    for (int i = 2; i < 7; i++)
+      Labels[i].setIcon(resize(getAnt(imagen_actual, i-2)));
+    for (int i = 7; i < Labels.length; i++)
+      Labels[i].setIcon(resize(getSig(imagen_actual, i-5)));
     imagen_actual = imagen_actual.getSig();
   }
     
-  public void move_back() {
-    lblBigImage.setIcon(getAnt(imagen_actual, 1));
-    
-    lblAct.setIcon(resize(getAnt(imagen_actual, 1)));
-    
-    lblAnt1.setIcon(resize(getAnt(imagen_actual, 2)));
-    lblAnt2.setIcon(resize(getAnt(imagen_actual, 3)));
-    lblAnt3.setIcon(resize(getAnt(imagen_actual, 4)));
-    lblAnt4.setIcon(resize(getAnt(imagen_actual, 5)));
-    lblAnt5.setIcon(resize(getAnt(imagen_actual, 6)));
-    
-    lblSig1.setIcon(resize(getSig(imagen_actual, 0)));
-    lblSig2.setIcon(resize(getSig(imagen_actual, 1)));
-    lblSig3.setIcon(resize(getSig(imagen_actual, 2)));
-    lblSig4.setIcon(resize(getSig(imagen_actual, 3)));
-    lblSig5.setIcon(resize(getSig(imagen_actual, 4)));
-    
+  public void move_back(JLabel Labels[]) {
+    Labels[0].setIcon(getAnt(imagen_actual, 1));
+    for (int i = 1; i < 7; i++)
+      Labels[i].setIcon(resize(getAnt(imagen_actual, i)));
+    for (int i = 7; i < Labels.length; i++) 
+      Labels[i].setIcon(resize(getSig(imagen_actual, i-7)));
     imagen_actual = imagen_actual.getAnt();
   }
   
   public ImageIcon resize(ImageIcon image) {
     Image img = image.getImage().getScaledInstance(100, 77, Image.SCALE_SMOOTH);
     return new ImageIcon(img);
+  }
+  
+  public JLabel[] getLabels() {
+    JLabel Labels[] = {lblBigImage, lblAct, 
+                       lblAnt1,     lblAnt2,  lblAnt3, lblAnt4, lblAnt5,
+                       lblSig1,     lblSig2,  lblSig3, lblSig4,  lblSig5};   
+    return Labels;      
   }
   
   public void addActionListeners() {
@@ -209,23 +183,20 @@ class ImageViewerFrame extends JFrame {
         }
       }
     });
-
     menu.add(exitItem);
     exitItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent event) {
         System.exit(0);
       }
     });
-    
     btn_next.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent event) {
-        if (imagen_actual.getSig() != null) move_next();
+        if (imagen_actual.getSig() != null) move_next(getLabels());
       }
     });
-    
     btn_back.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent event) {
-        if (imagen_actual.getAnt() != null) move_back();
+        if (imagen_actual.getAnt() != null) move_back(getLabels());
       }
     });
   }
