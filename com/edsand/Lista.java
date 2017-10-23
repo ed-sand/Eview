360eca986e9040e0fd4db9c39ff92058215598a1
Eduardo Sandoval <ed-sand@users.noreply.github.com>
2017-10-23 13:26:41 -0600

    Add files via upload

---
+++ com/edsand/Lista.java
@@ -0,0 +1,53 @@
+//Author: Jose Eduardo Sandoval Polanco
+
+package com.edsand;
+
+import javax.swing.ImageIcon;
+
+class Lista {
+  Imagen inicio, fin;
+  
+  public Lista() {
+    inicio = fin = null;
+  }
+  
+  public void meter(Imagen imagen) {
+    if (inicio == null) {
+      inicio = fin = imagen;
+      fin.setSig(inicio);
+      inicio.setAnt(fin);
+    } else {
+      fin.setSig(imagen);
+      fin.getSig().setAnt(fin);
+      fin = fin.getSig();
+      fin.setSig(inicio);
+      inicio.setAnt(fin);
+    }
+  }
+  
+  public void sacar() {
+    if (inicio == null) System.out.println("Lista vacia!");
+    else if (inicio == fin) inicio = fin = null;
+    else {
+      fin = fin.getAnt();
+      fin.setSig(inicio);
+    }
+  }
+  
+  public void paso_izq() {
+    Imagen tmp = inicio;
+    inicio = inicio.getSig();
+    meter(tmp);
+  }
+  
+  public void paso_der() {
+    Imagen tmp = fin;
+    fin = fin.getAnt();
+    inicio.setAnt(tmp);
+    inicio = tmp;
+  }
+  
+  public Imagen getInicio() {
+    return inicio;
+  }
+}
